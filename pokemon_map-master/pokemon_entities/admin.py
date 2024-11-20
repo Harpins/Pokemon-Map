d0c3651543064a26ea9c1cf1798d0eaebb27e5e1
Igor Derevnin <shroom2207@yandex.ru>
2024-11-20 07:46:06 +0300

    Delete pokemon_map-master directory

--- pokemon_map-master/pokemon_entities/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from pokemon_entities.models import Pokemon, PokemonEntity, Element
-admin.site.register(Pokemon)
-admin.site.register(PokemonEntity)
-admin.site.register(Element)
